4e27ec65138ee378e615d07da816e1ad5bbef983
wilsaj <wilson.andrew.j+github@gmail.com>
2015-08-28 08:32:27 -0500

    cln: drop unused import

--- scripts/uncollect-features.js
+++ scripts/uncollect-features.js
@@ -1,8 +1,6 @@
 // stream in feature collections -> stream out individual features
 
-import es from 'event-stream';
 import JSONStream from 'JSONStream';
-
 
 process.stdin
   .pipe(JSONStream.parse('features.*'))
