bd68f39bcf5245b23c06b6d8f63886de25c58d60
wilsaj <wilson.andrew.j+github@gmail.com>
2015-11-05 18:02:29 -0600

    add a /load_and_zoom JOSM remote API call to pre-load OSM data

--- scripts/add-import-properties.js
+++ scripts/add-import-properties.js
@@ -2,19 +2,25 @@
 // the json merged into the properties
 
 import es from 'event-stream';
+import extent from 'geojson-extent';
 import JSONStream from 'JSONStream';
 
 
 process.stdin
   .pipe(JSONStream.parse())
   .pipe(es.map(function(feature, cb) {
+    const bbox = extent(feature.geometry);
+
     const blockGroup = feature.properties.GEOID;
     const remoteControlBase = `http://localhost:8111`;
     const importBase = `${remoteControlBase}/import?url=https://github.com/wilsaj/atx-buildings/raw/with-import-data/osm/${blockGroup}`;
+    const loadAndZoomURL = `${remoteControlBase}/load_and_zoom?left=${bbox[0]}&bottom=${bbox[1]}&right=${bbox[2]}&top=${bbox[3]}`;
+
     feature.properties.import_comment = `<p>import links:</p>
-- <a href="${importBase}/buildings-to-import.osm">buildings-to-conflate</a>
-- <a href="${importBase}/addresses-to-import.osm">addresses-to-import</a>
-- <a href="${importBase}/addresses-to-conflate.osm">addresses-to-conflate</a>
+- <a target="_blank" href="${loadAndZoomURL}">load existing OSM data</a>
+- <a target="_blank" href="${importBase}/buildings-to-import.osm">buildings-to-conflate</a>
+- <a target="_blank" href="${importBase}/addresses-to-import.osm">addresses-to-import</a>
+- <a target="_blank" href="${importBase}/addresses-to-conflate.osm">addresses-to-conflate</a>
 </li>
 `;
 
